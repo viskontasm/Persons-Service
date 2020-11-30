789c6f57a66a6775efd61f77861a1282099893f3
Marius Viskontas <viskontasm@gmail.com>
2020-11-30 10:45:17 +0200

    first commit

---
+++ src/main/java/com/persons/demo/PersonsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.persons.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/persons/demo/PersonsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persons.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
